52798f68e89f4032e2498ef66c6894c969ab1e17
Jon Gracias <74846399+JonGracias@users.noreply.github.com>
2026-01-16 04:44:18 -0500

    Changed redirect back.

--- src/components/GitHubLogButton.tsx
+++ src/components/GitHubLogButton.tsx
@@ -86,7 +86,7 @@ export default function GitHubAuthButton({
     const router = useRouter();
 
   const handleAuth = () => {
-    const redirect = encodeURIComponent(window.location.origin + "/");
+    const redirect = encodeURIComponent("/");
 
     if (isLogged) {
       // SWA logout is a redirect, not a fetch
